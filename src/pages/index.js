5fa8c0a84383fde3c1dbdfe8d58b8ae9624a1ff4
LenLeg\EhabAyman <>
2025-08-21 21:53:03 +0300

    added page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -90,6 +90,7 @@ export default function Home() {
   return (
     <>
       <Head>
+        <title>Ehab Ayman - Lead Game & AR/VR Developer</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
